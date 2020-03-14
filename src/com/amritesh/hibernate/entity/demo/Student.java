67801829e17e855e4f186d75361421f4d3ff3ee7
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-14 13:57:11 +0530

    GenerationType

--- src/com/amritesh/hibernate/entity/demo/Student.java
+++ src/com/amritesh/hibernate/entity/demo/Student.java
@@ -2,8 +2,12 @@ package com.amritesh.hibernate.entity.demo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GeneratorType;
 
 @Entity
 @Table(name = "student")
@@ -18,6 +22,7 @@ public class Student {
 	}
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 	
